84e91c938b24b96e02e5eb982665c9ab22bc791e
owensks <91138060+owensks@users.noreply.github.com>
2023-05-17 13:57:59 +0000

    Latest build of target web.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,11 +10,182 @@ var ptx_lunr_docs = [
   "body": "   example.org   https:\/\/example.org   copyright  "
 },
 {
+  "id": "sec-homework-solutions",
+  "level": "1",
+  "url": "sec-homework-solutions.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Homework Solutions",
+  "body": " Homework Solutions  This will be where I put in some homework problems and solutions.   Techniques of Integration   TI1 Substitution Method  Explain and demonstrate how to find the anti-derivative of .       Explain and demonstrate how to evaluate the following definite integral:        was taken from Calculus for Team-Based Inquiry Learning.    TI2 Integration by Parts  Explain how to find the anti-derivative of .       Explain how to evaluate the following definite integral:         TI3 Integration of Trigonometry  Use trigonometric identities and to explain and demonstrate how to find .    .   Explain and demonstrate how to use double-angle identities to rewrite . (Do not solve the rewritten integral.)    .    TI4 Trigonometric Substitutino  Consider these given integrals of trignometric functions.              Use appropriate trignometric substitutions and the above integrals to find each of the following.       Substitute to find        Substitute to find        Substitute to find     TI5 Table of Integrals  Using a table of integrals, evaluate .        TI6 Partial Fractions  Explain and demonstrate how to use partial fractions to decompose .       Consider the partial fraction decomposition Use this decomposition to compute .         TI8 Improper Integrals  Explain and demonstrate how to write each of the following improper integrals as a limit, and why this limit coverges or diverges.       Diverges.       Diverges.       Converges.       Converges.     Applications of Integration   AI1 Average Values  Explain and demonstrate how to compute the average value of over the interval .   The average value of over the interval is     AI2 Arclength  Consider the curve defined on .   Estimate the arclength of this curve with line segments with .    The estimated length of this curve is 1.0191999999999999.   Find an integral that computes the arclength of this curve.   The arclength is computed via     AI3 Volumes of Revolution  Consider the region bounded by .   Find an integral which computes the volume of the solid formed by rotating this region about the -axis.   Using the washer\/disk method, we obtain and using the shell method, we obtain   Find an integral which computes the volume of the solid formed by rotating this region about the -axis.   Using the washer\/disk method, we obtain and using the shell method, we obtain     Sequences and Series   SQ1 Sequences  Find the first 5 terms, beginning with , of the sequence defined by the closed formula .       Find the first 5 terms of the sequence defined by the recursive formula .       Find a closed form for the following sequence:    Beginning with , let .   Find a recursive form for the following sequence:     .    SQ2 Sequence Properties and Limits  For each of the following sequences, determine which of the properties: monotonic , bounded and convergent , the sequence satisfies. If a sequence is convergent, determine to what it converges.   .     The sequence is monotonic.    The sequence is not bounded.    The sequence does not converge.     .     The sequence is monotonic.    The sequence is bounded.    The sequence converges to .      SQ3 Partial Sum Sequence  Consider the partial sum sequence    Find a closed form for .   .   Does converge? If so, to what value?    diverges.   Consider the partial sum sequence    Find a closed form for .   .   Does converge? If so, to what value?    converges to .    SQ4 Geometric Series  Find the limit of the following series.         SQ5 Basic Convergence Tests  For each of the following series, use the Divergence , Alternating Summation or Integral test to determine if the series converges.        diverges by the Divergence test.        converges by the Integral test.        converges by the Alternating Summation test.    SQ6 Comparison Tests  For each of the following series, determine if it converges or diverges, and explain your choice.        converges.        converges.    SQ7 Ratio and Root Tests  Use the ratio or root test to determine if converges or diverges.    diverges.    SQ8 Absolute Convergence  For each of the following series, determine if the series is convergent , and if the series is absolutely convergent .        converges and is absolutely convergent.        converges but is not absolutely convergent.    "
+},
+{
+  "id": "exercise-TI1",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI1",
+  "type": "Checkpoint",
+  "number": "1.1.1",
+  "title": "TI1 Substitution Method.",
+  "body": "TI1 Substitution Method  Explain and demonstrate how to find the anti-derivative of .       Explain and demonstrate how to evaluate the following definite integral:       "
+},
+{
+  "id": "exercise-TI2",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI2",
+  "type": "Checkpoint",
+  "number": "1.1.2",
+  "title": "TI2 Integration by Parts.",
+  "body": "TI2 Integration by Parts  Explain how to find the anti-derivative of .       Explain how to evaluate the following definite integral:       "
+},
+{
+  "id": "exercise-TI3",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI3",
+  "type": "Checkpoint",
+  "number": "1.1.3",
+  "title": "TI3 Integration of Trigonometry.",
+  "body": "TI3 Integration of Trigonometry  Use trigonometric identities and to explain and demonstrate how to find .    .   Explain and demonstrate how to use double-angle identities to rewrite . (Do not solve the rewritten integral.)    .  "
+},
+{
+  "id": "exercise-TI4",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI4",
+  "type": "Checkpoint",
+  "number": "1.1.4",
+  "title": "TI4 Trigonometric Substitutino.",
+  "body": "TI4 Trigonometric Substitutino  Consider these given integrals of trignometric functions.              Use appropriate trignometric substitutions and the above integrals to find each of the following.       Substitute to find        Substitute to find        Substitute to find   "
+},
+{
+  "id": "exercise-TI5",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI5",
+  "type": "Checkpoint",
+  "number": "1.1.5",
+  "title": "TI5 Table of Integrals.",
+  "body": "TI5 Table of Integrals  Using a table of integrals, evaluate .      "
+},
+{
+  "id": "exercise-TI6",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI6",
+  "type": "Checkpoint",
+  "number": "1.1.6",
+  "title": "TI6 Partial Fractions.",
+  "body": "TI6 Partial Fractions  Explain and demonstrate how to use partial fractions to decompose .       Consider the partial fraction decomposition Use this decomposition to compute .      "
+},
+{
+  "id": "exercise-TI8",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-TI8",
+  "type": "Checkpoint",
+  "number": "1.1.7",
+  "title": "TI8 Improper Integrals.",
+  "body": "TI8 Improper Integrals  Explain and demonstrate how to write each of the following improper integrals as a limit, and why this limit coverges or diverges.       Diverges.       Diverges.       Converges.       Converges.  "
+},
+{
+  "id": "exercise-AI1",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-AI1",
+  "type": "Checkpoint",
+  "number": "1.1.8",
+  "title": "AI1 Average Values.",
+  "body": "AI1 Average Values  Explain and demonstrate how to compute the average value of over the interval .   The average value of over the interval is   "
+},
+{
+  "id": "exercise-AI2",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-AI2",
+  "type": "Checkpoint",
+  "number": "1.1.9",
+  "title": "AI2 Arclength.",
+  "body": "AI2 Arclength  Consider the curve defined on .   Estimate the arclength of this curve with line segments with .    The estimated length of this curve is 1.0191999999999999.   Find an integral that computes the arclength of this curve.   The arclength is computed via   "
+},
+{
+  "id": "exercise-AI3",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-AI3",
+  "type": "Checkpoint",
+  "number": "1.1.10",
+  "title": "AI3 Volumes of Revolution.",
+  "body": "AI3 Volumes of Revolution  Consider the region bounded by .   Find an integral which computes the volume of the solid formed by rotating this region about the -axis.   Using the washer\/disk method, we obtain and using the shell method, we obtain   Find an integral which computes the volume of the solid formed by rotating this region about the -axis.   Using the washer\/disk method, we obtain and using the shell method, we obtain  "
+},
+{
+  "id": "exercise-SQ1",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ1",
+  "type": "Checkpoint",
+  "number": "1.1.11",
+  "title": "SQ1 Sequences.",
+  "body": "SQ1 Sequences  Find the first 5 terms, beginning with , of the sequence defined by the closed formula .       Find the first 5 terms of the sequence defined by the recursive formula .       Find a closed form for the following sequence:    Beginning with , let .   Find a recursive form for the following sequence:     .  "
+},
+{
+  "id": "exercise-SQ2",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ2",
+  "type": "Checkpoint",
+  "number": "1.1.12",
+  "title": "SQ2 Sequence Properties and Limits.",
+  "body": "SQ2 Sequence Properties and Limits  For each of the following sequences, determine which of the properties: monotonic , bounded and convergent , the sequence satisfies. If a sequence is convergent, determine to what it converges.   .     The sequence is monotonic.    The sequence is not bounded.    The sequence does not converge.     .     The sequence is monotonic.    The sequence is bounded.    The sequence converges to .    "
+},
+{
+  "id": "exercise-SQ3",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ3",
+  "type": "Checkpoint",
+  "number": "1.1.13",
+  "title": "SQ3 Partial Sum Sequence.",
+  "body": "SQ3 Partial Sum Sequence  Consider the partial sum sequence    Find a closed form for .   .   Does converge? If so, to what value?    diverges.   Consider the partial sum sequence    Find a closed form for .   .   Does converge? If so, to what value?    converges to .  "
+},
+{
+  "id": "exercise-SQ4",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ4",
+  "type": "Checkpoint",
+  "number": "1.1.14",
+  "title": "SQ4 Geometric Series.",
+  "body": "SQ4 Geometric Series  Find the limit of the following series.       "
+},
+{
+  "id": "exercise-SQ5",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ5",
+  "type": "Checkpoint",
+  "number": "1.1.15",
+  "title": "SQ5 Basic Convergence Tests.",
+  "body": "SQ5 Basic Convergence Tests  For each of the following series, use the Divergence , Alternating Summation or Integral test to determine if the series converges.        diverges by the Divergence test.        converges by the Integral test.        converges by the Alternating Summation test.  "
+},
+{
+  "id": "exercise-SQ6",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ6",
+  "type": "Checkpoint",
+  "number": "1.1.16",
+  "title": "SQ6 Comparison Tests.",
+  "body": "SQ6 Comparison Tests  For each of the following series, determine if it converges or diverges, and explain your choice.        converges.        converges.  "
+},
+{
+  "id": "exercise-SQ7",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ7",
+  "type": "Checkpoint",
+  "number": "1.1.17",
+  "title": "SQ7 Ratio and Root Tests.",
+  "body": "SQ7 Ratio and Root Tests  Use the ratio or root test to determine if converges or diverges.    diverges.  "
+},
+{
+  "id": "exercise-SQ8",
+  "level": "2",
+  "url": "sec-homework-solutions.html#exercise-SQ8",
+  "type": "Checkpoint",
+  "number": "1.1.18",
+  "title": "SQ8 Absolute Convergence.",
+  "body": "SQ8 Absolute Convergence  For each of the following series, determine if the series is convergent , and if the series is absolutely convergent .        converges and is absolutely convergent.        converges but is not absolutely convergent.  "
+},
+{
   "id": "section-title",
   "level": "1",
   "url": "section-title.html",
   "type": "Section",
-  "number": "1.1",
+  "number": "2.1",
   "title": "Section title",
   "body": " Section title  Hello  "
 },
@@ -23,9 +194,36 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "sec-this-is-a-great-section.html",
   "type": "Section",
-  "number": "2.1",
+  "number": "3.1",
   "title": "This is a great section",
   "body": " This is a great section  This is a great paragraph!  This is going to be the picture of a rectangle, once I get it working.  A picture of a small rectangle     "
+},
+{
+  "id": "sec-homework",
+  "level": "1",
+  "url": "sec-homework.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Homework",
+  "body": " Homework  Emphasizing donut because they are my favorite breakfast food item. Meanwhile, my favorite math formula is or The definition of important vocab words will come later.   A circle is a parallelogram with sides of equal length.   My three favorite numbers are:             Threety     Steven really likes BIG math equations so I will write one here:   "
+},
+{
+  "id": "p-103",
+  "level": "2",
+  "url": "sec-homework.html#p-103",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "vocab "
+},
+{
+  "id": "def-circle",
+  "level": "2",
+  "url": "sec-homework.html#def-circle",
+  "type": "Definition",
+  "number": "3.2.1",
+  "title": "",
+  "body": "  A circle is a parallelogram with sides of equal length.   "
 },
 {
   "id": "colophon-2",
